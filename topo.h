66a183838ca383c338e5d6c193f6292b1acf5f5e
Linnéa Blomstrand <linneablomstran@gmail.com>
2026-01-10 13:10:09 +0100

    kommentarer ändrade

--- topo.h
+++ topo.h
@@ -3,7 +3,7 @@
 
 #include "graph.h"
 
-// Returns 1 if DAG and fills order[0..n-1], else 0 if cycle.
+// Returns 1 if DAG and fills order[0..n-1], 0 if cycle.
 int kahn_toposort(const Graph *g, int *order);
 
 #endif
